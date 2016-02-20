f54980cb15e2e393b61a1ba46aadbc2ecc03fb23
Dmitry Chernyshov <dmitry@razortheory.com>
2016-02-20 21:44:38 +0300

    Tiling an image

--- script.js
+++ script.js
@@ -15,15 +15,13 @@ function draw(){
     }
 
     var img = new Image();
-    img.src = 'https://mdn.mozillademos.org/files/5395/backdrop.png';
+    img.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
     img.onload = function(){
-      ctx.drawImage(img, 0, 0);
-      ctx.beginPath();
-      ctx.moveTo(30, 96);
-      ctx.lineTo(70, 66);
-      ctx.lineTo(103, 76);
-      ctx.lineTo(170, 15);
-      ctx.stroke();
+      for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 3; j++) {
+          ctx.drawImage(img, j * 50, i * 38, 50, 38);
+        }
+      }
     }
 
   } else {

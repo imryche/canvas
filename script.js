3685b0d32854dce117f6da60fbaa93df926d8051
Dmitry Chernyshov <dmitry@razortheory.com>
2016-02-20 21:16:57 +0300

    Forgotten js file

---
+++ script.js
@@ -0,0 +1,51 @@
+function draw(){
+  var canvas = document.getElementById('tutorial');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+    var scaleFactor = backingScale(ctx);
+    if (scaleFactor > 1) {
+      var width = canvas.width;
+      var height = canvas.height;
+      canvas.width = width * scaleFactor;
+      canvas.height = height * scaleFactor;
+      canvas.style.width = width + 'px';
+      canvas.style.height = height + 'px';
+      //update context for scaled canvas
+      ctx.scale(scaleFactor, scaleFactor);
+    }
+
+    ctx.font = '48px Roboto';
+    ctx.strokeText('Hello world', 10, 50);
+
+  } else {
+    // canvas-unsupported code
+    console.log('Sorry, your browser doesn\'t support canvas');
+  }
+}
+
+function getRandomColor(){
+  var rgb = [];
+  var min = 0;
+  var max = 255;
+  for (var i=0; i<3; i++){
+    var randInt = Math.round(Math.random() * (max - min) + min);
+    rgb.push(randInt);
+  }
+  return 'rgb(' + rgb.join() + ')';
+}
+
+function getRandomPos(canvas, context){
+  var scale = backingScale(context);
+  var randX = Math.random() * (canvas.width - 0) + 0;
+  var randY = Math.random() * (canvas.height - 0) + 0;
+  return [Math.round(randX / scale), Math.round(randY / scale)];
+}
+
+function backingScale(context){
+  if ('devicePixelRatio' in window) {
+    if (window.devicePixelRatio > 1){
+      return window.devicePixelRatio;
+    }
+  }
+  return 1;
+}

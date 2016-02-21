bf9d0c799ca3d7ba73d606f7a6597da494bb3661
Dmitry Chernyshov <dmitry@razortheory.com>
2016-02-21 19:44:58 +0300

    Slice an image

--- script.js
+++ script.js
@@ -14,15 +14,11 @@ function draw(){
       ctx.scale(scaleFactor, scaleFactor);
     }
 
-    var img = new Image();
-    img.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
-    img.onload = function(){
-      for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 3; j++) {
-          ctx.drawImage(img, j * 50, i * 38, 50, 38);
-        }
-      }
-    }
+    // Draw slice
+    ctx.drawImage(document.getElementById('source'),
+                  33, 71, 104, 124, 21, 20, 87, 104);
+    // Draw frame
+    ctx.drawImage(document.getElementById('frame'), 0, 0);
 
   } else {
     // canvas-unsupported code

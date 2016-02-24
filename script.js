7276408f048cb6af086dbdd04fbaac66c3c24c5f
Dmitry Chernyshov <dmitry@razortheory.com>
2016-02-24 22:29:57 +0300

    Transformational matrix

--- script.js
+++ script.js
@@ -14,18 +14,25 @@ function draw(){
       ctx.scale(scaleFactor, scaleFactor);
     }
 
-    // draw a simple rectangle, but scale it
-    ctx.save();
-    ctx.scale(10, 3);
-    ctx.fillRect(1, 10, 10, 10);
-    ctx.restore();
-
-    // mirror horizontally
-    ctx.scale(-1, 1);
-    ctx.font = '48px Roboto';
-    ctx.fillText("MDN", -135, 120);
+    var sin = Math.sin(Math.PI / 24);
+    var cos = Math.cos(Math.PI / 24);
+    ctx.translate(100, 100);
+    var c = 0;
+    ctx.globalAlpha = 1;
 
 
+    for (var i = 0; i < 48; i++) {
+      c = Math.floor(255 / 24 * i);
+      ctx.fillStyle = 'rgb(' + c + ',' + c + ',' + c + ')';
+      ctx.fillStyle = getRandomColor();
+      ctx.globalAlpha -= ctx.globalAlpha / 24;
+      ctx.fillRect(0, 0, 100, 10);
+      ctx.transform(cos, sin, -sin, cos, 0, 0);
+    }
+
+    ctx.setTransform(-1, 0, 0, 1, 100, 100);
+    ctx.fillStyle = 'rgba(255, 128, 255, 0.5)'
+    ctx.fillRect(0, 50, 100, 100);
   } else {
     // canvas-unsupported code
     console.log('Sorry, your browser doesn\'t support canvas');

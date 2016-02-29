b7e0eb128a8bb30c5ea42c440384030d06ddc54e
Dmitry Chernyshov <dmitry@razortheory.com>
2016-02-29 22:28:23 +0300

    Advanced animation

--- script.js
+++ script.js
@@ -1,70 +1,68 @@
-var sun = new Image();
-var moon = new Image();
-var earth = new Image();
+var canvas = document.getElementById('canvas');
+var leftOffset = canvas.offsetLeft;
+var ctx = canvas.getContext('2d');
+var raf;
+var running = false;
 
-function init(){
-  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
-  window.requestAnimationFrame(draw);
+var ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 2,
+  radius: 25,
+  color: getRandomColor(),
+  draw: function(){
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
+
+function clear(){
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function draw(){
-  var canvas = document.getElementById('tutorial');
-  if (canvas.getContext){
-    var ctx = canvas.getContext('2d');
-    /*
-    var scaleFactor = backingScale(ctx);
-    if (scaleFactor > 1) {
-      var width = canvas.width;
-      var height = canvas.height;
-      canvas.width = width * scaleFactor;
-      canvas.height = height * scaleFactor;
-      canvas.style.width = width + 'px';
-      canvas.style.height = height + 'px';
-      //update context for scaled canvas
-      ctx.scale(scaleFactor, scaleFactor);
-    }
-    */
+  clear();
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
 
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, 300, 300);
-
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-    ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-    ctx.save();
-    ctx.translate(150, 150);
-
-    // earth
-    var time = new Date();
-    ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-    ctx.translate(105, 0);
-    ctx.fillRect(0, -12, 50, 24); // Shadow
-    ctx.drawImage(earth, -12, -12);
-
-    // moon
-    ctx.save();
-    ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-    ctx.translate(0, 28.5);
-    ctx.drawImage(moon, -3.5, -3.5);
-    ctx.restore();
-
-    ctx.restore()
-
-    ctx.beginPath();
-    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); //Earth orbit
-    ctx.stroke();
-
-    ctx.drawImage(sun, 0, 0, 300, 300);
-
-    window.requestAnimationFrame(draw);
-  } else {
-    // canvas-unsupported code
-    console.log('Sorry, your browser doesn\'t support canvas');
+  if (ball.y + ball.vy + ball.radius > canvas.height || ball.y + ball.vy - ball.radius < 0){
+    ball.vy = -ball.vy;
   }
+  if (ball.x + ball.vx + ball.radius > canvas.width || ball.x + ball.vx - ball.radius < 0){
+    ball.vx = -ball.vx;
+  }
+
+  raf = window.requestAnimationFrame(draw);
 }
 
-init();
+canvas.addEventListener('mousemove', function(e){
+  if (!running){
+    clear();
+    ball.x = e.clientX - leftOffset;
+    ball.y = e.clientY;
+    ball.draw();
+  }
+});
+
+canvas.addEventListener('click', function(e){
+  if (!running){
+    raf = window.requestAnimationFrame(draw);
+    running = true;
+  }
+});
+
+canvas.addEventListener('mouseout', function(e){
+  window.cancelAnimationFrame(raf);
+  running = false;
+});
+
+ball.draw();
 
 function getRandomColor(){
   var rgb = [];
